17be85c28f1c3dc4affd1d7abe669ebada4a85fb
Tupal-cell <55320896+Tupal-cell@users.noreply.github.com>
2020-04-06 15:05:26 +0200

    onclick+style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import ResultComponent from './components/ResultComponent';
-import KeyPadComponent from "./components/KeyPadComponent";
+import ResultComponent from '../src/components/ResultComponent';
+import KeyPadComponent from '../src/components/KeypadComponent';
 
 class App extends Component {
     constructor(){
@@ -11,6 +11,50 @@ class App extends Component {
             result: ""
         }
     }
+    onClick = button => {
+
+      if(button === "="){
+          this.calculate()
+      }
+
+      else if(button === "C"){
+          this.reset()
+      }
+      else if(button === "CE"){
+          this.backspace()
+      }
+
+      else {
+          this.setState({
+              result: this.state.result + button
+          })
+      }
+  };
+
+  calculate = () => {
+      try {
+          this.setState({
+              result: (eval(this.state.result) || "" ) + ""
+          })
+      } catch (e) {
+          this.setState({
+              result: "error"
+          })
+
+      }
+  };
+
+  reset = () => {
+      this.setState({
+          result: ""
+      })
+  };
+
+  backspace = () => {
+      this.setState({
+          result: this.state.result.slice(0, -1)
+      })
+  };
 
     render() {
         return (
